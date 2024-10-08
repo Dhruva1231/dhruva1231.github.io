fd498f193e7662ee238af4553d49e834c0e29e1a
Dhruva1231 <62621689+Dhruva1231@users.noreply.github.com>
2024-10-08 19:54:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9455,17 +9455,6 @@
  * @license MIT
  */
 
-const modelViewer = document.querySelector('.model-container model-viewer');
-
-// Add hover event listeners
-modelViewer.addEventListener('mouseenter', () => {
-    modelViewer.style.transform = 'translateY(-20px)'; // Move up on hover
-});
-
-modelViewer.addEventListener('mouseleave', () => {
-    modelViewer.style.transform = 'translateY(0)'; // Reset position when hover ends
-});
-
 
 /*! Bundled license information:
  
