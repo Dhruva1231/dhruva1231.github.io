82dda838b63cb7197edb01acd4b94d3f90cf545d
Dhruva1231 <62621689+Dhruva1231@users.noreply.github.com>
2024-10-08 21:52:26 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,15 +1,1 @@
-alert('working');
 
-// Ensure the DOM is fully loaded before running the script
-document.addEventListener('DOMContentLoaded', function () {
-    const modelViewer = document.querySelector('.model-container model-viewer');
-
-    // Add hover event listeners
-    modelViewer.addEventListener('mouseenter', () => {
-        modelViewer.style.transform = 'translateY(-20px)'; // Move up on hover
-    });
-
-    modelViewer.addEventListener('mouseleave', () => {
-        modelViewer.style.transform = 'translateY(0)'; // Reset position when hover ends
-    });
-});
